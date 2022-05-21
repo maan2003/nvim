5f9a0eee76320313497a6c7392c8c48a328c65fa
Maan2003 <manmeetmann2003@gmail.com>
2022-05-21 18:47:14 +0530

    Remove lsp-facination

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -49,42 +49,6 @@ use {
    requires = 'kyazdani42/nvim-web-devicons',
 }
 
--- doesn't work :no_mouth:
-use {
-   'windwp/lsp-fastaction.nvim',
-   config = function()
-      require('lsp-fastaction').setup {
-         hide_cursor = true,
-         action_data = {
-            --- action for filetype dart
-            ['dart'] = {
-               -- pattern is a lua regex with lower case
-               { pattern = 'import library', key = 'i', order = 1 },
-               { pattern = 'wrap with widget', key = 'w', order = 2 },
-               { pattern = 'wrap with column', key = 'c', order = 3 },
-               { pattern = 'wrap with row', key = 'r', order = 3 },
-               { pattern = 'wrap with sizedbox', key = 's', order = 3 },
-               { pattern = 'wrap with container', key = 'C', order = 4 },
-               { pattern = 'wrap with center', key = 'E', order = 4 },
-               { pattern = 'padding', key = 'P', order = 4 },
-               { pattern = 'wrap with streambuilder', key = 'S', order = 5 },
-               { pattern = 'remove', key = 'R', order = 5 },
-
-               --range code action
-               { pattern = "surround with %'if'", key = 'i', order = 2 },
-               { pattern = 'try%-catch', key = 't', order = 2 },
-               { pattern = 'for%-in', key = 'f', order = 2 },
-               { pattern = 'setstate', key = 's', order = 2 },
-            },
-            ['typescript'] = {
-               { pattern = 'to existing import declaration', key = 'a', order = 2 },
-               { pattern = 'from module', key = 'i', order = 1 },
-            },
-         },
-      }
-   end,
-}
-
 use {
     'williamboman/nvim-lsp-installer',
 }
