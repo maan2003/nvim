323fbddc73a9efdd737dcb016acce78013c97932
Maan2003 <manmeetmann2003@gmail.com>
2022-05-26 10:28:47 +0530

    remove lsp_lines

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -82,10 +82,3 @@ use ({
         })
     end
 })
-
-use({
-  "https://git.sr.ht/~whynothugo/lsp_lines.nvim",
-  config = function()
-    -- require("lsp_lines").register_lsp_virtual_lines()
-  end,
-})
