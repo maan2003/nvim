9874857ec728c1deee90d5f63a9b39d833004b87
Maan2003 <manmeetmann2003@gmail.com>
2022-06-02 23:51:41 +0530

    Handle cmp not installed gracefully

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -1,8 +1,5 @@
 local use = require('packer').use
 
-use 'hrsh7th/nvim-cmp'
-use 'hrsh7th/cmp-nvim-lsp'
-use 'L3MON4D3/LuaSnip'
 
 vim.cmd [[set completeopt=menu,menuone,noselect]]
 
@@ -11,49 +8,58 @@ local has_words_before = function()
    return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match '%s' == nil
 end
 
-local cmp = require 'cmp'
-local luasnip = require 'luasnip'
+use 'L3MON4D3/LuaSnip'
+use 'hrsh7th/cmp-nvim-lsp'
 
-cmp.setup {
-   snippet = {
-      expand = function(args)
-         luasnip.lsp_expand(args.body)
-      end,
-   },
-   mapping = cmp.mapping.preset.insert {
-      ['<C-d>'] = cmp.mapping.scroll_docs(-4),
-      ['<C-f>'] = cmp.mapping.scroll_docs(4),
-      ['<C-Space>'] = cmp.mapping.complete(),
-      ['<C-e>'] = cmp.mapping.close(),
-      ['<CR>'] = cmp.mapping.confirm { select = true },
-      ['<Tab>'] = cmp.mapping(function(fallback)
-         if cmp.visible() then
-            cmp.select_next_item()
-         elseif luasnip.expand_or_jumpable() then
-            luasnip.expand_or_jump()
-         elseif has_words_before() then
-            cmp.complete()
-         else
-            fallback()
-         end
-      end, { 'i', 's' }),
-      ['<S-Tab>'] = cmp.mapping(function(fallback)
-         if cmp.visible() then
-            cmp.select_prev_item()
-         elseif luasnip.jumpable(-1) then
-            luasnip.jump(-1)
-         else
-            fallback()
-         end
-      end, { 'i', 's' }),
-   },
-   sources = {
-      { name = 'nvim_lsp' },
-   },
-   completion = {
-      get_trigger_characters = function(chars)
-         return chars
-      end,
-      keyword_length = 2,
-   },
+use {
+   'hrsh7th/nvim-cmp',
+   config = function() 
+      local cmp = require 'cmp'
+      local luasnip = require 'luasnip'
+
+      cmp.setup {
+         snippet = {
+            expand = function(args)
+               luasnip.lsp_expand(args.body)
+            end,
+         },
+         mapping = cmp.mapping.preset.insert {
+            ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+            ['<C-f>'] = cmp.mapping.scroll_docs(4),
+            ['<C-Space>'] = cmp.mapping.complete(),
+            ['<C-e>'] = cmp.mapping.close(),
+            ['<CR>'] = cmp.mapping.confirm { select = true },
+            ['<Tab>'] = cmp.mapping(function(fallback)
+               if cmp.visible() then
+                  cmp.select_next_item()
+               elseif luasnip.expand_or_jumpable() then
+                  luasnip.expand_or_jump()
+               elseif has_words_before() then
+                  cmp.complete()
+               else
+                  fallback()
+               end
+            end, { 'i', 's' }),
+            ['<S-Tab>'] = cmp.mapping(function(fallback)
+               if cmp.visible() then
+                  cmp.select_prev_item()
+               elseif luasnip.jumpable(-1) then
+                  luasnip.jump(-1)
+               else
+                  fallback()
+               end
+            end, { 'i', 's' }),
+         },
+         sources = {
+            { name = 'nvim_lsp' },
+         },
+         completion = {
+            get_trigger_characters = function(chars)
+               return chars
+            end,
+            keyword_length = 2,
+         },
+      }
+   end
 }
+
