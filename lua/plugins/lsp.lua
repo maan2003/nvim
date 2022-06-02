016ffc14c8fe69d4cb880a60998939b432680715
Maan2003 <manmeetmann2003@gmail.com>
2022-06-02 23:01:40 +0530

    inline config for nvim-lsp-installer

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -57,24 +57,16 @@ use {
 }
 
 use {
-    'williamboman/nvim-lsp-installer',
+   'williamboman/nvim-lsp-installer',
+   config = function() 
+      local lsp_installer = require("nvim-lsp-installer")
+
+      lsp_installer.on_server_ready(function(server)
+         local opts = {}
+         server:setup(opts)
+      end)
+   end
 }
-local lsp_installer = require("nvim-lsp-installer")
-
--- Register a handler that will be called for all installed servers.
--- Alternatively, you may also register handlers on specific server instances instead (see example below).
-lsp_installer.on_server_ready(function(server)
-    local opts = {}
-
-    -- (optional) Customize the options passed to the server
-    -- if server.name == "tsserver" then
-    --     opts.root_dir = function() ... end
-    -- end
-
-    -- This setup() function is exactly the same as lspconfig's setup function.
-    -- Refer to https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
-    server:setup(opts)
-end)
 
 use ({
     'onsails/diaglist.nvim',
