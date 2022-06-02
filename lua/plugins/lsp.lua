e4e2cdca6f5b9924a24966811d59398bd66251a3
Maan2003 <manmeetmann2003@gmail.com>
2022-06-02 23:02:38 +0530

    Remove diaglist.nvim

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -68,16 +68,4 @@ use {
    end
 }
 
-use ({
-    'onsails/diaglist.nvim',
-    config = function()
-        require("diaglist").init({
-            -- optional settings
-            -- below are defaults
-            debug = false,
 
-            -- increase for noisy servers
-            debounce_ms = 150,
-        })
-    end
-})
