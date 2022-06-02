1530fcf24d84aa0a565da5460fd67cc2dc1b79b0
Maan2003 <manmeetmann2003@gmail.com>
2022-06-02 23:00:35 +0530

    use cargo check instead of clippy

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -39,7 +39,7 @@ use {
             settings = {
                ['rust-analyzer'] = {
                   checkOnSave = {
-                     command = 'clippy',
+                     command = 'check',
                   },
                   cargo = {
                      runBuildScript = true,
