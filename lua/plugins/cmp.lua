aee4685fd2acf4a0a59feda52f61df79146145f0
Maan2003 <manmeetmann2003@gmail.com>
2022-06-13 09:30:17 +0530

    Don't use upvalues in config

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -2,11 +2,6 @@ local use = require('packer').use
 
 vim.cmd [[set completeopt=menu,menuone,noselect]]
 
-local has_words_before = function()
-   local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-   return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match '%s' == nil
-end
-
 use 'L3MON4D3/LuaSnip'
 use 'hrsh7th/cmp-nvim-lsp'
 
@@ -15,6 +10,11 @@ use {
    config = function()
       local cmp = require 'cmp'
       local luasnip = require 'luasnip'
+      local has_words_before = function()
+         local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+         return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match '%s' == nil
+      end
+
 
       cmp.setup {
          snippet = {
